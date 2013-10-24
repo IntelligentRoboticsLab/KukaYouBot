ccb52bc35012a88574f68b151307713802fa03a7
S.J.R. van Schaik <stephan@synkhronix.com>
2013-10-24 10:23:52 +0100

    Added autotools support to libtmcl.

--- libtmcl/include/tmcl/tmcl_defs.h
+++ libtmcl/include/tmcl/tmcl_defs.h
@@ -1,17 +1,17 @@
 /*
  * This file is part of libtmcl.
  *
- * libskro is free software: you can redistribute it and/or modify it under the
+ * libtmcl is free software: you can redistribute it and/or modify it under the
  * terms of the GNU General Public License as published by the Free Software
  * Foundation, either version 3 of the License, or (at your option) any later
  * version.
  *
- * libskro is distributed in the hope that it will be useful, but WITHOUT ANY
+ * libtmcl is distributed in the hope that it will be useful, but WITHOUT ANY
  * WARRANTY; without even the implied warranty of MERCHANTIBILITY or FITNESS FOR
  * A PARTICULAR PURPOSE. See the GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License along with
- * libskro. If not, see <http://www.gnu.org/licenses/>.
+ * libtmcl. If not, see <http://www.gnu.org/licenses/>.
  */
 
 #ifndef TMCL_DEFS_H
